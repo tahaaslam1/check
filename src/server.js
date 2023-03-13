e3ce72b6b94bf327d4f4faaa32afa3cdd6ac8867
Taha Aslam <76480940+tahaaslam1@users.noreply.github.com>
2023-03-13 20:47:02 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Ab hujao please');
+  res.send('Ab hujao please dhek');
 });
 
 app.listen(PORT, HOST, () => {
